b4ca97f2f6249dd9546b63eb5f2901f57f910785
Trooy Thompson <tr-09@hotmail.com>
2023-05-31 21:24:45 -0600

    initial commit

---
+++ PrioritizedDayBuilder/Data/PriorityTask.cs
@@ -0,0 +1,12 @@
+﻿namespace PriortizedDayBuilder.Data
+{
+    public class PriorityTask
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Day { get; set; }
+        public int Priority { get; set; }
+        public double Hours { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
